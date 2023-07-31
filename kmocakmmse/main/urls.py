e892b8bb303d493d20ba67c306e85679751c1e54
sungmoon <smk980510@naver.com>
2023-07-31 22:57:47 +0900

    django start

---
+++ kmocakmmse/main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from main import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'myapp'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('info/', views.info, name='info'),
+    path('details/', views.details, name='details'),
+    path('confirm/', views.confirm, name='confirm'),
+    path('cutoff/', views.cutoff, name='cutoff'),
+    path('machin/', views.machin, name='machin'),
+]
